3086114394e50c27e35ebcf8011b598d4e605575
Roman Savrulin <romansavrulin@gmail.com>
2018-06-29 18:31:02 +0300

    improve test to use 2 GPIO in ISR mode

--- main.cc
+++ main.cc
@@ -7,72 +7,108 @@
 #include <unistd.h> // usleep()
 
 using namespace std::chrono;
-
-
-
-high_resolution_clock::time_point beg;
-duration<double, std::micro> accum;
+using namespace std;
 
 
 class Handler
 {
 public:
-   void handle(GPIO::Value val)
+	Handler(int id):_g(id, GPIO::Edge::BOTH, std::bind(&Handler::handle, this, std::placeholders::_1, std::placeholders::_2)){
+		_value = _g.getValue();
+		_accum = std::chrono::duration<double, std::micro>(0.0);
+	}
+   void handle(unsigned short id, GPIO::Value val)
    {
+	   _value = val;
       const high_resolution_clock::time_point end = high_resolution_clock::now();
 
-      const auto time_span = duration_cast<microseconds>(end - beg);
-      accum += time_span;
-      std::cout << "Latency: " << time_span.count() << " microseconds" << std::endl;
+      const auto time_span = duration_cast<microseconds>(end - _beg);
+      _accum += time_span;
+      std::cout << "id: " << id << "; Val: " << val << "; Latency: " << time_span.count() << " microseconds" << std::endl;
    }
+
+   void startTimeout(){
+	   _beg = high_resolution_clock::now();
+   }
+
+   void waitValueISR(GPIO::Value v){
+   	auto timeout = 100;//00000;
+   	while(_value != v && timeout--){
+   		//cout << "_v: " << h._value << "; v: " << v << endl;
+   		usleep(10000);
+   	}
+
+
+   	if(_value != v){
+   		cout << "Timeout waiting " << v << "on " << _g.id() << endl;
+   		throw std::runtime_error("Timeout waiting " + std::to_string(v));
+   	}else
+   		cout << "WaitValueISR " << _g.id() << " Got: " << v << endl;
+   }
+
+protected:
+   GPIO _g;
+   high_resolution_clock::time_point _beg;
+   duration<double, std::micro> _accum;
+
+   volatile int _value = GPIO::LOW;
 };
 
-
-void myisr(GPIO::Value val)
-{
-   const high_resolution_clock::time_point end = high_resolution_clock::now();
-
-   const auto time_span = end - beg;
-   accum += time_span;
-   std::cout << "Latency: " << time_span.count() << " microseconds" << std::endl;
+void waitValuePoll(GPIO &g1, GPIO &g2){ //TODO: implement
+	//return ;
+	//auto v1 = GPIO::Value::LOW;
+	auto v1 = g1.getValue();
+	auto v2 = g2.getValue();
+	 //if (v1 != v2)
+		 cout << "Set " << v1 << "; Read " << v2 << endl;
 }
-
 
 int main()
 {
-   Handler h;
-
-   accum = std::chrono::duration<double, std::micro>(0.0);
-
-
-
-
-   // Member functions do not take any longer to call than global functions
-   std::function<void(GPIO::Value)> global(myisr);
-
-   std::function<void(GPIO::Value)> handleisr =
-      std::bind(&Handler::handle, &h, std::placeholders::_1);
 
    {
       // Short GPIO 15 (input) to GPIO 27 (output) for the following latency test
-      GPIO gpio1(27, GPIO::Direction::OUT);
-      GPIO gpio2(15, GPIO::Edge::RISING, handleisr); // will be destroyed first,
-                                                     // so no spurious call to handleisr upon
-                                                     // destruction of GPIO 27
+      GPIO gpio1(395, GPIO::Direction::OUT);
+      Handler h(391);
+      Handler h2(396);
 
       usleep(125000);
 
-      const unsigned int nIterations = 50;
+      gpio1.setValue(GPIO::LOW);
+      usleep(125000);
+      gpio1.setValue(GPIO::HIGH);
+      usleep(125000);
+      gpio1.setValue(GPIO::LOW);
+      usleep(125000);
+
+      cout << "Main Cycle" << endl;
+
+      const unsigned int nIterations = 50000;
       for(unsigned int i=0;i<nIterations;++i)
       {
-         beg = high_resolution_clock::now();
+    	  cout << "High" << endl;
+
          gpio1.setValue(GPIO::HIGH);
-         usleep(31250);
+
+         h.startTimeout();
+         h2.startTimeout();
+         h.waitValueISR(GPIO::HIGH);
+         h2.waitValueISR(GPIO::LOW);
+         //waitValuePoll(gpio1, gpio2);
+         //usleep(3125000);
+
+         cout << "Low" << endl;
 
          gpio1.setValue(GPIO::LOW);
-         usleep(31250);
+
+         h.startTimeout();
+         h2.startTimeout();
+         h.waitValueISR(GPIO::LOW);
+         h2.waitValueISR(GPIO::HIGH);
+         //waitValuePoll(gpio1, gpio2);
+         //usleep(3125000);
       }
 
-      std::cout << "Average: " << accum.count()/nIterations << " microseconds " << std::endl;
+      //std::cout << "Average: " << _accum.count()/nIterations << " microseconds " << std::endl;
    }
 }
